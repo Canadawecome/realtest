50ca969195fdb2faeeb98dcc89d4a03a58e3b429
vuongnguyentat <vuong.nguyentat28@gmail.com>
2023-06-27 13:54:52 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,58 +9,41 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <PowerBIEmbed
-          embedConfig={
-            {
-              type: 'report', // Supported types: report, dashboard, tile, visual, and qna.
-              id: '71530fca-a9f0-45a4-8527-420b614ac002',
-              embedUrl: "https://app.powerbi.com/reportEmbed?reportId=71530fca-a9f0-45a4-8527-420b614ac002&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVNPVVRILUVBU1QtQVNJQS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsibW9kZXJuRW1iZWQiOnRydWUsInVzYWdlTWV0cmljc1ZOZXh0Ijp0cnVlfX0%3d",
-              accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvMDZmMWI4OWYtMDdlOC00NjRmLWI0MDgtZWMxYjQ1NzAzZjMxLyIsImlhdCI6MTY4Nzc0ODE5MiwibmJmIjoxNjg3NzQ4MTkyLCJleHAiOjE2ODc3NTI5MTcsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFtSVl0VnVQYTNudytmU3NzcU5EaTV3dW5DUmxmTEdyUVYvN1dORlYyU3hGRng4TTVicnNlRnRXYTBWUGRuOXFXIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImlwYWRkciI6IjExOC43MC4xNTYuNDMiLCJuYW1lIjoiTmd1eWVuIFRhdCBWdW9uZyAyMDIwNjMxMyIsIm9pZCI6ImJiZjhjMTFmLWI2ZmYtNGZmNC04MDBkLTM1YjIxYmM5MTQ4ZiIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0yNzQ2MjUxMDA3LTEzMjQ1OTUyMDYtNzgxNjU0MzUxLTc5NDgxIiwicHVpZCI6IjEwMDMyMDAwRUExODYzMzUiLCJyaCI6IjAuQVhJQW43anhCdWdIVDBhMENPd2JSWEFfTVFrQUFBQUFBQUFBd0FBQUFBQUFBQUJ5QUVJLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6ImE1YUVIVlJPdEZHeHVMNzFaRHA4X1lieXZ1aGt3dkh3MXowQmRfeFBHN3ciLCJ0aWQiOiIwNmYxYjg5Zi0wN2U4LTQ2NGYtYjQwOC1lYzFiNDU3MDNmMzEiLCJ1bmlxdWVfbmFtZSI6IlZ1b25nLk5UMjA2MzEzQHNpcy5odXN0LmVkdS52biIsInVwbiI6IlZ1b25nLk5UMjA2MzEzQHNpcy5odXN0LmVkdS52biIsInV0aSI6IlNZb0JPOE9PVlVhTlVTSUpPdlFZQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.h1DrHVQQIhsBqpZHXx4cW1pUmM-Jod4S4ngDsRgaDiW_CMDToK1c2Oi7l_Z_9M4CMcmoy0yLBBZ7ViIjiUmQWDQbAtlvBE5sCwukVx6XwXdQ8GFA7zVFAiQwoVfIJIXCPlpT1iAGLWi7Ir4RiJCc3gCIHNyZS4fpgXBXKGTArV5s87C0O6l_9KtVyoIFOe_UZB-1Hp30EIVlaffw4PFFkK6Q4_uX_t1L-9Uz_VPi33LCePsLgwcnXicghi6wJnPimfM0_MHnR4tneWxz7rzLuvoqjgs9Z_rKZsOy08EQj9Tvz_OwFomBxyhOEt-yjSP_a1dsjbhqIE1S0bBOuT1xgg',
-              tokenType: models.TokenType.Embed, // Use models.TokenType.Aad if you're embedding for your organization.
-              settings: {
-                panes: {
-                  filters: {
-                    expanded: false,
-                    visible: false
-                  }
-                },
-              }
+          embedConfig={{
+            type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+            id: 'bbe141f2-4cdd-4fa1-9d2c-644726f4c445',
+            embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=bbe141f2-4cdd-4fa1-9d2c-644726f4c445&groupId=090cfcf9-c81b-4a61-9e17-11f663f35ff6&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVNPVVRILUVBU1QtQVNJQS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsibW9kZXJuRW1iZWQiOnRydWUsInVzYWdlTWV0cmljc1ZOZXh0Ijp0cnVlfX0%3d',
+            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvMDZmMWI4OWYtMDdlOC00NjRmLWI0MDgtZWMxYjQ1NzAzZjMxLyIsImlhdCI6MTY4Nzg0NzI0MCwibmJmIjoxNjg3ODQ3MjQwLCJleHAiOjE2ODc4NTI5MTMsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFLRVBScDJLakllamhTTHBHTTZQckdVM0hqSFFuQVM2aUxrQjJwaHR5WEphMlNuSnk3cVk1S1QzZEFvZmdIaHlFIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImlwYWRkciI6IjExOC43MC4xMjcuMTYwIiwibmFtZSI6Ik5ndXllbiBUYXQgVnVvbmcgMjAyMDYzMTMiLCJvaWQiOiJiYmY4YzExZi1iNmZmLTRmZjQtODAwZC0zNWIyMWJjOTE0OGYiLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtMjc0NjI1MTAwNy0xMzI0NTk1MjA2LTc4MTY1NDM1MS03OTQ4MSIsInB1aWQiOiIxMDAzMjAwMEVBMTg2MzM1IiwicmgiOiIwLkFYSUFuN2p4QnVnSFQwYTBDT3diUlhBX01Ra0FBQUFBQUFBQXdBQUFBQUFBQUFCeUFFSS4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJhNWFFSFZST3RGR3h1TDcxWkRwOF9ZYnl2dWhrd3ZIdzF6MEJkX3hQRzd3IiwidGlkIjoiMDZmMWI4OWYtMDdlOC00NjRmLWI0MDgtZWMxYjQ1NzAzZjMxIiwidW5pcXVlX25hbWUiOiJWdW9uZy5OVDIwNjMxM0BzaXMuaHVzdC5lZHUudm4iLCJ1cG4iOiJWdW9uZy5OVDIwNjMxM0BzaXMuaHVzdC5lZHUudm4iLCJ1dGkiOiJPS09ka1ExMG4wV0QwNU03SEV3S0FBIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXX0.VadCnI21RBdBB3rbiJBdM6bnMrq-l2I1aB1ls4NSfWvpfJJAzKJxHo8qF-w0GwbnKYsB6G8NbNYU4Rh5h5wfjS4oqMWdIP95ldMiub69cpWjwExOXv34owAPMaEmlMpea7b5AAxLqE7C4qRLQYW2a0-3jxnmkR2aRedLWJRAdxpW2VucjJNaaX7nSeOnGKK8SzelOwA8BZaD7DMORxkNObvQPAl6o8OwKNA4htMtQ20POrrQGvnREzGfGDAqXBn__3uAQeHTei4Fu7HK4A0lfbTQqs7DVadJY0YCRLMpsEsGJ9GUPHiVTYfCFRFgvFVAL26pvTbcSAyT-9Anjldi3w',
+            tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
+            settings: {
+              panes: {
+                filters: {
+                  expanded: false,
+                  visible: false
+                }
+              },
+
             }
-          }
+          }}
 
           eventHandlers={
             new Map([
-              ['loaded', function () {
-                console.log('Report loaded');
-              }],
-              ['rendered', function () {
-                console.log('Report rendered');
-              }],
-              ['error', function (event) {
-                console.log(event.detail);
-              }]
+              ['loaded', function () { console.log('Report loaded'); }],
+              ['rendered', function () { console.log('Report rendered'); }],
+              ['error', function (event) { console.log(event.detail); }],
+              ['visualClicked', () => console.log('visual clicked')],
+              ['pageChanged', (event) => console.log(event)],
             ])
           }
 
-          cssClassName={
-            "report-style-class"
-          }
+          cssClassName={"Embed-container"}
 
-          getEmbeddedComponent={
-            (embeddedReport) => {
-              window.report = embeddedReport;
-            }
-          }
+          getEmbeddedComponent={(embeddedReport) => {
+            window.report = embeddedReport;
+          }}
         />
-        embedConfig = {
-          {
-            type: 'report', // Supported types: report, dashboard, tile, visual, and qna.
-            id: undefined,
-            embedUrl: undefined,
-            accessToken: undefined, // Keep as an empty string, null, or undefined.
-            tokenType: models.TokenType.Embed
-          }
-        }
       </header>
     </div>
   );
